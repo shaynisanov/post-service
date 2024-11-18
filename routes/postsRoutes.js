a834abe0a075f5e032483703703ef450e49e711a
shay <shay5nis@gmail.com>
2024-11-18 21:25:18 +0200

    add getPostById route

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {getAllPosts, addNewPost} from "../controllers/postsController.js";
+import {getAllPosts, addNewPost, getPostById} from "../controllers/postsController.js";
 
 const router = express.Router();
 
 router.get("/", getAllPosts);
 router.post("/", addNewPost);
+router.get("/:id", getPostById);
 
 export {router as postRouter}
